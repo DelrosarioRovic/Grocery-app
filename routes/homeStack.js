e731b82577e59fef98d469aa631df165f7ca9010
DelrosarioRovic <rovicdelrosario01@gmail.com>
2024-01-06 10:53:49 +0800

    adding login and finish login screen

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,7 +1,10 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { TouchableOpacity, Image } from "react-native";
 import Intro from "../screen/intro";
 import Login from "../screen/login";
+import arrowLeft from "../assets/ep_arrow-left-bold.png";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,7 +17,21 @@ const Route = () => {
           component={Intro}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={({ navigation }) => ({
+            title: null, // Set the title as null to have an empty title
+            headerLeft: () => (
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Image source={arrowLeft} style={{ width: 30, height: 30 }} />
+              </TouchableOpacity>
+            ),
+
+            headerShadowVisible: false,
+          })}
+        />
+        {/* Add more screens as needed */}
       </Stack.Navigator>
     </NavigationContainer>
   );
